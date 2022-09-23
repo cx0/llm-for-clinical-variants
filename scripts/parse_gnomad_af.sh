4db7619a4a312053cb9a00201e22c493167fc9f4
onuralp <onuralp@gmail.com>
2022-09-22 20:53:15 -0700

    Extract gnomAD population allele frequencies.

--- scripts/parse_gnomad_af.sh
+++ scripts/parse_gnomad_af.sh
@@ -6,7 +6,8 @@ wget https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1.1/liftov
 wget https://storage.googleapis.com/gcp-public-data--gnomad/release/2.1.1/liftover_grch38/vcf/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.vcf.bgz.tbi
 
 # Create a file with positions of interest in `bed` format
-awk -F'\t' '{if (NR!=1) print "chr"$1"\t"$2}' ../data/final_mutant_list.tsv > ../data/final_mutant_list_grch38_pos.txt
+# Append REF/ALT for downstream processing
+awk -F'\t' '{if (NR!=1) print "chr"$1"\t"$2"\t"$4\t"$5"}' ../data/final_mutant_list.tsv > ../data/final_mutant_list_grch38_pos.txt
 
 # Query gnomAD variant dataset for the target sites
 echo "CHROM\tPOS\tID\tREF\tALT\tFILTER\tAC\tAN\tAF\tAF_popmax\tpopmax\tallele_type\tvariant_type\tn_alt_alelles\tvep" > ../data/final_mutant_list_gnomad_allele_freq.txt
